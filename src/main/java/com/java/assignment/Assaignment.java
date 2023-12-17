e37e753548e92b441e38669dc08490cf82cb263c
K V S H Ganesh <kvsh.ganesh@gmail.com>
2023-12-18 01:02:50 +0530

    Initial commit

---
+++ src/main/java/com/java/assignment/Assaignment.java
@@ -0,0 +1,16 @@
+package com.java.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Assaignment extends SpringBootServletInitializer{
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Assaignment.class, args);
+	}
+	
+	
+}
